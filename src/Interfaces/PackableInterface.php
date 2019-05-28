d445dfe5a985dcab2e508618a44545263b37eb58
ueef <artem@arkadev.org>
2019-05-28 15:23:44 +0500

    PackableInterface::unpack must return object

--- src/Interfaces/PackableInterface.php
+++ src/Interfaces/PackableInterface.php
@@ -6,9 +6,5 @@ namespace Ueef\Packable\Interfaces;
 interface PackableInterface
 {
     public function pack(): array;
-
-    /**
-     * @return PackableInterface
-     */
-    public static function unpack(array $values);
+    public static function unpack(array $packed): object;
 }
