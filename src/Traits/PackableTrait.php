aeec264785bb6ce7b3f7efcd6bd2a9f517790aa8
ueef <artem@arkadev.org>
2020-03-03 15:47:28 +0500

    add a second argument to PackableTrait::unpack as ?PackableInterface $item = null

--- src/Traits/PackableTrait.php
+++ src/Traits/PackableTrait.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Ueef\Packable\Traits;
 
+use Exception;
 use Ueef\Packable\Interfaces\PackableInterface;
 
 trait PackableTrait
 {
-    public static function unpack(array $packed): PackableInterface
+    public static function unpack(array $packed, ?PackableInterface $item = null): PackableInterface
     {
-        $item = new static();
+        if (null === $item) {
+            $item = new static;
+        } elseif (!$item instanceof static) {
+            throw new Exception(sprintf("the item must be compatible with %s", static::class));
+        }
+
         foreach ($packed as $key => $value) {
             if (property_exists($item, $key)) {
                 $item->{$key} = $value;
